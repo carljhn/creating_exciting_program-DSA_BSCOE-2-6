79bc10ae21256c9a47e01124152dfc850a7e6add
Carljhn <zoletacarljohn1@gmail.com>
2022-11-19 12:50:54 -1200

    Initial commit: I added the seatwork's instructions

---
+++ excitingProgram.py
@@ -0,0 +1,6 @@
+#              Seatwork
+# - Create a program of your choice.
+# - You may get inspiration from YouTube.com
+# - Your should include the purpose/idea of the program and where can it be applied.
+# - Upload the demo in Messenger before 4pm
+# - Include the link / reference of your program in README or comment in the code
